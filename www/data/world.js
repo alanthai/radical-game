d8aa5dd2a68a414d356381cb66c19a0f202c8b53
alanthai <winged.cloudchaser@gmail.com>
2015-11-30 16:02:01 -0500

    ?

--- www/data/world.js
+++ www/data/world.js
@@ -2,5 +2,5 @@ define(["exports", "module"], function (exports, module) {
   "use strict";
 
   module.exports = [{}, // skip level 0
-  { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 4 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 5 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 6 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 10 }];
+  { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 3 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 4 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 5 }, { enemies: ["shadowbat", "hauntedjelly", "jiangshi"], killsRequired: 10 }];
 });
